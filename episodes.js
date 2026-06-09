655c80da08773b333853f68bea381e66c819d430
paulsorlut-source <paul.sorlut@gmail.com>
2026-06-09 03:09:57 +0100

    Update episodes.js

--- episodes.js
+++ episodes.js
@@ -3,7 +3,7 @@ const episodes = [
     season: "Saison 1",
     episode: "Épisode 1",
     date: "27/11/2023",
-    title: "Les voisins qui baisent et les gâteaux mous.",
+    title: "Podchiasse : les voisins qui baisent et les gâteaux mous.",
     link: "https://shows.acast.com/we-love-tftc/episodes/podchiasse-les-voisins-qui-baisent-et-les-gateaux-mous",
     guests: [
       {
@@ -35,7 +35,7 @@ const episodes = [
         season: "Saison 1",
         episode: "Épisode 2",
         date: "28/12/2023",
-        title: "Les branlettes collectives et les frites dans les grilles",
+        title: "Podchiasse : les branlettes collectives et les frites dans les grilles.",
         link: "https://shows.acast.com/we-love-tftc/episodes/podchiasse-les-branlettes-collectives-et-les-frites-dans-les",
         guests: [
           {
@@ -62,6 +62,69 @@ const episodes = [
               "Les notifications inutiles"
               "Les branlettes collectives"
               "Les additions partagées"
+            ]
+          },
+        ]
+      },
+  {
+        season: "Saison 1",
+        episode: "Épisode 3",
+        date: "25/01/2024",
+        title: "Podchiasse : les devoirs et les Aérobus avec Léopold Lemarchand.",
+        link: "https://shows.acast.com/we-love-tftc/episodes/podchiasse-les-devoirs-et-les-aerobus",
+        guests: [
+          {
+            name: "Jean-Baptiste Toussaint",
+            subjects: [
+              "Les devoirs",
+              "Enlever ses chaussures étant invité",
+              "Les cagnottes"
+            ]
+          },
+          {
+            name: "Léopold Lemarchand",
+            subjects: [
+              "Les aérobus",
+              "Les textos tout en minuscule",
+              "Mettre ses potes en story",
+            ]
+          },
+          {
+            name: "Mehdi",
+            subjects: [
+              "Les serviettes en papier",
+              "James Bond différents"
+              "Les bloqueurs de trottoirs"
+            ]
+          },
+        ]
+      },
+    {
+        season: "Saison 1",
+        episode: "Épisode 4",
+        date: "15/02/2024",
+        title: "Podchiasse : les managers Mcdo et les glands avec Léopold Lemarchand.",
+        link: "https://shows.acast.com/we-love-tftc/episodes/podchiasse-les-managers-mcdo-et-les-glands",
+        guests: [
+          {
+            name: "Jean-Baptiste Toussaint",
+            subjects: [
+              "Les serveurs charmeurs",
+              "Les silences de fin de séance",
+            ]
+          },
+          {
+            name: "Léopold Lemarchand",
+            subjects: [
+              "Les continueurs de blagues",
+              "Continuer son footing au feu rouge",
+            ]
+          },
+          {
+            name: "Mehdi",
+            subjects: [
+              "Les managers Mcdo",
+              "Les Cocas sans bulles"
             ]
           },
         ]
