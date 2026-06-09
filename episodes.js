0809ec8f949bd4fb91519aa4e36543b39cfe6f3e
paulsorlut-source <paul.sorlut@gmail.com>
2026-06-09 02:20:39 +0100

    Create episodes.js

---
+++ episodes.js
@@ -0,0 +1,18 @@
+const episodes = [
+  {
+    season: "Saison 1",
+    episode: "Épisode 1",
+    date: "01/01/2024",
+    title: "Titre de l'épisode",
+    link: "https://lien-vers-episode.com",
+    guests: [
+      {
+        name: "Nom de l'invité",
+        subjects: [
+          "Premier sujet qui l'énerve",
+          "Deuxième sujet qui l'énerve"
+        ]
+      }
+    ]
+  }
+];
