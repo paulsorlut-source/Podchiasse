7b8dcd655633114d1234406f1023aa7148b088ff
paulsorlut-source <paul.sorlut@gmail.com>
2026-06-14 20:06:58 +0100

    Create data-template.js

---
+++ data-template.js
@@ -0,0 +1,19 @@
+// Template à copier-coller dans episodes.js pour ajouter un nouvel épisode
+
+{
+  season: "Saison 1",
+  episode: "Épisode 1",
+  date: "JJ/MM/AAAA",
+  title: "Titre de l'épisode",
+  link: "https://...",
+  guests: [
+    {
+      name: "Nom de l'invité",
+      subjects: [
+        "Sujet 1",
+        "Sujet 2",
+        "Sujet 3"
+      ]
+    }
+  ]
+},
